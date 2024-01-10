59185f6395e263887aad489368838cd1f7a28087
yunyami <yunyami0605@gmail.com>
2024-01-10 17:03:56 +0900

    feat: msw 셋팅, 백엔드 코드 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import {MSWComponent} from "@/app/_component/MSWComponent";
+import { MSWComponent } from "@/app/_component/MSWComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <MSWComponent /> */}
+        <MSWComponent />
         {children}
       </body>
     </html>
